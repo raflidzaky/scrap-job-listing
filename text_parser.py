638456687a395cf2cd12d6a6121e92ea838b6e06
raflidzaky <raflidzakyansyah@gmail.com>
2024-11-01 15:07:15 +0700

    Refactor the code logic and documentation

--- text_parser.py
+++ text_parser.py
@@ -18,58 +18,81 @@ def get_jobs():
     jobs = job_cat.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
     return jobs
+
+def get_details(job_input:str): 
+    '''
+        This func will get a details from each job listing, such as skillset, position, and firms.
+        Input: jobs (from get_jobs func) 
+        Output: Job details as a return (list data structure)
+    '''
+
+    # Initialize variables as returns
+    job_details = [] # It will hold every details from each company's details
+
+    # The details of each individual company will be hold in a dictionary. The dictionary will map
+    # company name, company's position needed, and skills needed.
+    # This is done because I need to track the skills that belonging to each company name and position
+    # Appending altogether within a list will be extremely hard to track.
+    # Each company's details (n-company) will be put in job_details' list (n-index)
+
+    for job in job_input:
+        '''
+        =====================================================================================
+            This func will iterate each job listing to get its details. 
+            TASK 1: Find all of the company name and iterate it to put within the list
+        =====================================================================================
+        '''
+
+        # Initialize job info (or details) per iteration
+        job_info = {
+            'company': '',
+            'position': '',
+            'skills': []
+        }
+
+        # Find the company name
+        comp_name = job.find_all('h3', class_='joblist-comp-name')
+        for comp in comp_name:
+            job_info['company'] = comp.text.strip()
     
-def write_txt(job_input:str): 
-    with open('result.txt', 'w') as f:
-    # We want to find company name inside the job variable
-        for job in job_input:
-            comp_name = job.find_all('h3', class_='joblist-comp-name')
-            list_comp = []
-
-            for comp in comp_name:
-                list_comp.append(comp.text.strip())
-            # print(comp_name.text.strip())
-
+            '''
+            =====================================================================================
+                TASK 2: Find all of the position from each company. 
+                Since I take the position within company name iteration, I will no need to do any
+                iteration to get each position associated from job listing. 
+            =====================================================================================
+            '''
             # Parse the position
             position_listing = job.find('h2', class_='heading-trun')
             if position_listing:
+                # If there is position posted, get the text
                 position_text  = position_listing.text.strip()
                 position_part  = ' '.join(position_text.split()[:2])
                 if ':' in position_part:
-                    position_clean = position_part.replace(':', '')
-                
+                    job_info['position'] = position_part.replace(':', '')
+            else:
+                # Otherwise, just continue it
+                continue
 
-            # I also want to find the skills needed
+            '''
+            =====================================================================================
+                TASK 3: Find all of the skills related to each position from each company. 
+                Since I take the position within company name iteration, I will no need to do any
+                iteration to get each position associated from job listing. 
+            =====================================================================================
+            '''
             skill_section = job.find('div', class_='more-skills-sections')
 
+            # If there exists skill-section from job listing, find "skills" component
             if skill_section:
                 skills = skill_section.find_all('span')
 
-            list_skill = []
-            for skill in skills:
-                if '** ' in skill.text:
-                    cleaned_skill = skill.text.replace('** ', '')
-                    list_skill.append(cleaned_skill.strip())
-                elif ' **' in skill.text:
-                    cleaned_skill = skill.text.replace(' **', '')
-                    list_skill.append(cleaned_skill.strip())
-                else:
-                    list_skill.append(skill.text.strip())
-        
-            # Since write does not accept list input,
-            # I have to join it as a string
-            f.write("Companies:\n")
-            f.write("\n".join(list_comp) + "\n")  # Join the list into a single string with line breaks
-            f.write(position_clean + "\n")
-            f.write("\n")
-            f.write("Skills:\n")
-
-            for i, s in enumerate(list_skill):
-                f.write(f"{i+1}. {s}\n")
-            f.write("========\n\n")
-
-    print('File is being successfully written')
-   
-
-if __name__ == "__main__":
-    write_txt(get_jobs())
+            # Get each skills (multiple skills) and append it to the list. 
+                for skill in skills:
+                    cleaned_skill = skill.text.strip().replace('** ', '').replace(' **', '')
+                    job_info['skills'].append(cleaned_skill)  # Append each cleaned skill
+            
+            # Append each detail to job_details' list
+            job_details.append(job_info)
+            
+    return job_details
